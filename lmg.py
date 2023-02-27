6dd9a1529f14c4a1c16d586c3c050cc629ebed21
Ken Robbins <122292243+krobbinsgit@users.noreply.github.com>
2023-02-27 12:58:38 -0500

    Update lmg.py

--- lmg.py
+++ lmg.py
@@ -6,20 +6,20 @@ import numpy as np
 # First we need to define the state-prep circuit.
 
 def state_prep(angles):
-  M=len(angles)
-  blank=Circuit(M)
+  M=len(angles) # The total number of qubits required which is roughly equal to the number of particles N/2
+  blank=Circuit(M,M) #A blank circuit with M qubits and M bits. Not sure if we need the bits.
   with blank.context as (q,c):
-    gt.RY(angle[0],q[0])
-    for k in range(1,len(angles)):
+    gt.RY(angle[0],q[0]) # Apply a rotation gate to the first qubit
+    for k in range(1,len(angles)): # Apply controlled rotation gates to the rest of the qubits (control one below)
       gt.CRY(angles[k],q[k-1],q[k]) # Unsure if this is the syntax
   return(blank)
 
 # Now we need to write functions for the measurement diagonalization circuits
 
-def clique_2_diag(circ,num_qubits):
+def clique_2_diag(circ,num_qubits): # The post-prep circuit required to diagonalize the state for the measurement basis. Works with Clique 2 (see literature)
   with circ.context as (q,c):
     for k in range(num_qubits):
-      gt.H(q[k])
+      gt.H(q[k]) # Clique 1 is all Xs so we just apply Hadamards to each qubit.
   return(circ)
     
 def clique_3_diag(circ,num_qubits):
