1f3480e7559af22c278d20219dfef6028bf25cb5
Ken Robbins <122292243+krobbinsgit@users.noreply.github.com>
2023-03-02 14:20:05 -0500

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -1,3 +1,4 @@
 # This is the file which will have the python code to interpret my results from lmg.py's all_my_circuits() function.
+# At the end, it should plot a single-shot energy estimate histogram of data with vertical lines for theoretical value and data mean.
 import numpy as np
 from lmg.py import all_my_circuits
