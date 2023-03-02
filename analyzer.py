399587992a9d563705c56d22132ba25f39b60d33
Ken Robbins <122292243+krobbinsgit@users.noreply.github.com>
2023-03-02 14:17:26 -0500

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -1,1 +1,3 @@
-# This script will take in outputs from the lmg.py and interpret them correctly to make distributions of single-shot energy estimates.
+# This is the file which will have the python code to interpret my results from lmg.py's all_my_circuits() function.
+import numpy as np
+from lmg.py import all_my_circuits
